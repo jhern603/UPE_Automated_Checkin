08a4810e35015d94049f8bcb43feacd69977a872
Jose Hernandez <jhern603@fiu.edu>
2022-04-27 09:41:08 -0400

    Begin API buiilding

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  const { method } = req;
+
+  switch (method) {
+    case 'POST':
+      res.json({ method: 'POST', endpoint: 'test' });
+      break;
+    default:
+      res.setHeader('Allow', ['GET', 'POST']);
+      res.status(405).end(`Method ${method} Not Allowed`);
+      break;
+  }
 }
